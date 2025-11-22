4e0527b63e2c0a2208eeee65bc19de9a8e776620
Thomson-Lam <thomson.lam327@gmail.com>
2025-11-22 02:48:04 -0500

    checkpoint: initial frontend websocket; TODO: websocket dummy backend

--- frontend/src/app/demo/page.tsx
+++ frontend/src/app/demo/page.tsx
@@ -1,15 +1,18 @@
 import Chat from "@/components/DemoChat";
+import StreamingTerminal from "@/components/StreamingTerminal";
 
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-3 gap-4 h-screen">
-      <div className="col-span-2 bg-gray-200">
-<Chat />
-			</div>
-      <div className="grid grid-rows-2 gap-4">
-        <div className="bg-gray-400"></div>
-        <div className="bg-gray-600"></div>
+    <div className="grid grid-cols-3 gap-0 h-screen">
+      <div className="col-span-2 bg-gray-200 border-r-4 border-black">
+		<Chat />
+	</div>
+      <div className="grid grid-rows-2 gap-0">
+        <div className="bg-gray-400 border-b-4 border-black"></div>
+        <div className="bg-gray-600">
+			<StreamingTerminal />
+		</div>
       </div>
     </div>
   );
