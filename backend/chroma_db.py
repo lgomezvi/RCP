ece4dc0f8381d893e66d3bb5e12e35032a490af0
Eunsung Kim <ekim433@uwo.ca>
2025-11-22 13:16:43 -0500

    fix requirements.txt

--- backend/chroma_db.py
+++ backend/chroma_db.py
@@ -1,5 +1,28 @@
 import chromadb
-from chromadb.config import Settings
+from sentence_transformers import SentenceTransformer
 
-client = chromadb.PersistentClient("./chroma_robot")
-collection = client.get_or_create_collection("robot_knowledge")
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def embed(texts):
+    return model.encode(texts).tolist()
+
+client = chromadb.PersistentClient(path="./chroma-data")
+
+collection = client.get_or_create_collection(
+    name="robot_rag",
+    embedding_function=embed
+)
+
+# """ move to another file later"""
+# import json
+
+# def retrieve_context_from_chroma(json_instruction: dict):
+#     query_text = json.dumps(json_instruction)
+    
+#     results = collection.query(
+#         query_texts=[query_text],
+#         n_results=5
+#     )
+
+#     docs = results["documents"][0] if len(results["documents"]) > 0 else []
+#     return "\n\n".join(docs)
