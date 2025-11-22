d57e8be0a0c1fa0c60963a214e85059871b90bbb
Thomson-Lam <thomson.lam327@gmail.com>
2025-11-22 02:27:56 -0500

    checkpoint: layout for the demo page

--- frontend/src/app/demo/page.tsx
+++ frontend/src/app/demo/page.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
-	return (
-	<h1>DEMO</h1>
-
-	);
-
+  return (
+    <div className="grid grid-cols-3 gap-4 h-screen">
+      <div className="col-span-2 bg-gray-200"></div>
+      <div className="grid grid-rows-2 gap-4">
+        <div className="bg-gray-400"></div>
+        <div className="bg-gray-600"></div>
+      </div>
+    </div>
+  );
 }
